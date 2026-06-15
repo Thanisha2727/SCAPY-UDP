0957d68e3a742d6fbd8aace892c0bf2615591f73
Thanisha S <213471345+Thanisha2727@users.noreply.github.com>
2026-06-15 22:14:31 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 from scapy.all import *
 
-# Destination details
 target_ip = "127.0.0.1"
 target_port = 12345
 
@@ -8,12 +7,9 @@ while True:
     msg = input("Enter message (type 'exit' to stop): ")
 
     if msg.lower() == "exit":
-        break
 
-    # Build UDP packet
     packet = IP(dst=target_ip) / UDP(dport=target_port) / Raw(load=msg)
-
-    # Send packet
+\
     send(packet)
 
     print("Packet sent!")
